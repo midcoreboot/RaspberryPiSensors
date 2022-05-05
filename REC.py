e87a6a06d77535f18fcd00738fb1b0da06cb444d
Rasmus Kolmodin <rasmus.kolmodin@gmail.com>
2022-05-05 12:18:07 +0200

    added rec

---
+++ REC.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import json
+
+class Observation():
+    def __init__(self, observationTime, numericValue, stringValue, booleanValue, sensorId):
+        self.observationTime = observationTime.strftime("%m/%d/%Y, %H:%M:%S")
+        self.Value = numericValue
+        self.stringValue = stringValue
+        self.booleanValue = booleanValue
+        self.sensorId = sensorId
+
+class RecEdgeMessage():
+    def __init__(self, deviceId, observations):
+        self.format = "rec3.2"
+        self.deviceId = deviceId
+        self.observations = observations
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    
